e44df21acf41229e09502709b7819939560d07e5
ayushy78 <ayushy78@gmail.com>
2024-04-01 13:11:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/navbar/Navbar";
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
+<<<<<<< Updated upstream
 import Contact from "./components/contact/Contact";
 
 
@@ -12,6 +13,8 @@ import Contact from "./components/contact/Contact";
 
 
 
+=======
+>>>>>>> Stashed changes
 
 const App = () => {
   return <div>
@@ -19,6 +22,7 @@ const App = () => {
       <Navbar/>
       <Hero/>
       </section>
+<<<<<<< Updated upstream
     <section id="Services"><Parallax type="services"/></section>
     <section><Services/></section>
     <section id="Portfolio"><Parallax type="portfolio"/></section>
@@ -26,6 +30,26 @@ const App = () => {
     <section id="Contact"><Contact/></section>
     
     </div>;
+=======
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
+      <section>
+        <Services />
+      </section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
+      {/* Framer Motion Crash Course */}
+      {/* <Test/>
+    <Test/> */}
+    </div>
+  );
+>>>>>>> Stashed changes
 };
 
 export default App
