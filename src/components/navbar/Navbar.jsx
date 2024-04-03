91b41e135119b47e2ec11a9ea0213d2cb1860c0a
ayushy78 <ayushy78@gmail.com>
2024-04-03 18:21:55 +0530

    fyyh

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -17,16 +17,16 @@ const Navbar = () => {
         </motion.span>
         <div className="social">
           <a href="#">
-            <img src="/facebook.png" alt="" />
+            <img src="/PwerryInsta.png" alt="" />
           </a>
           <a href="#">
-            <img src="/instagram.png" alt="" />
+            <img src="/PwerryMedium.png" alt="" />
           </a>
           <a href="#">
-            <img src="/youtube.png" alt="" />
+            <img src="/PwerryTelegram.png" alt="" />
           </a>
           <a href="#">
-            <img src="/dribbble.png" alt="" />
+            <img src="/PwerryTwitter.png" alt="" />
           </a>
         </div>
       </div>
