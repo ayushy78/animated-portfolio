1b2a8644fc5a09780ec4407c481eda4d8ab8cf02
Aman Gupta <amanguptaprogrammer@gmail.com>
2024-04-04 23:41:21 +0530

    add images

--- src/ImageCarousel.jsx
+++ src/ImageCarousel.jsx
@@ -1,35 +1,59 @@
-import React from 'react'; 
-import 'bootstrap/dist/css/bootstrap.css'; 
-import Carousel from 'react-bootstrap/Carousel'; 
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Carousel from "react-bootstrap/Carousel";
 
-export default function App() { 
-return ( 
-	<div style={{ display: 'block', width: 700, padding: 30 }}> 
-	<h4>React-Bootstrap Carousel Component</h4> 
-	<Carousel> 
-		<Carousel.Item interval={1500}> 
-		<img 
-			className="d-block w-100"
-src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
-			alt="Image One"
-		/> 
-		<Carousel.Caption> 
-			<h3>Label for first slide</h3> 
-			<p>Sample Text for Image One</p> 
-		</Carousel.Caption> 
-		</Carousel.Item> 
-		<Carousel.Item interval={500}> 
-		<img 
-			className="d-block w-100"
-src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
-			alt="Image Two"
-		/> 
-		<Carousel.Caption> 
-			<h3>Label for second slide</h3> 
-			<p>Sample Text for Image Two</p> 
-		</Carousel.Caption> 
-		</Carousel.Item> 
-	</Carousel> 
-	</div> 
-); 
+// jsobect to store the images in public folder
+const images = [
+  {
+    id: 1,
+    src: "photo1.png",
+  },
+  {
+    id: 2,
+    src: "photo2.png",
+  },
+  {
+    id: 3,
+    src: "photo3.png",
+  },
+  {
+    id: 4,
+    src: "photo4.png",
+  },
+  {
+    id: 5,
+    src: "photo5.png",
+  },
+];
+
+export default function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <div style={{ display: "block", width: 700, padding: 30 }}>
+        <h4 style = {{textAlign: "center"}}>Story Of Pwerry The Platypus</h4>
+        <Carousel>
+          {images.map((image) => (
+            <Carousel.Item key={image.id} interval={1500}>
+              <img
+                className="d-block w-100"
+                src={image.src}
+                alt={`Image ${image.id}`}
+              />
+              {/* <Carousel.Caption>
+              <h3>Label for slide {image.id}</h3>
+              <p>Sample Text for Image {image.id}</p>
+            </Carousel.Caption> */}
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      </div>
+    </div>
+  );
 }
