6aec7fdbc68e1d84edb5813912fa51a26c2e3801
Aman Gupta <amanguptaprogrammer@gmail.com>
2024-04-05 16:32:05 +0530

    commit

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,6 @@
 import { useRef } from "react";
 import "./services.scss";
 import { motion, useInView } from "framer-motion";
-import photo2 from './photo2.png';
-
 
 const variants = {
   initial: {
@@ -23,7 +21,34 @@ const variants = {
 
 const phases = {
   phase1: {
-    id: 1,  
+    id: 1,
+    style: {
+      backgroundImage: "url('./Roadmap.png')",
+      "&::before": {
+        content: "",
+        display: "block",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        backgroundImage: "url('./photo4.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        opacity: 0,
+        transition: "opacity 1s",
+        zIndex: 1,
+      },
+      "&:hover::before": {
+        opacity: 0,
+      },
+    },
+    whileHover: { backgroundImage: "url('./photo3.png')" },
+    title: "Branding",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum libero enim nisi aliquam consectetur expedita magni eius ex corrupti animi! Ad nam pariatur assumenda quae mollitia libero repellat explicabo maiores?",
+  },
+  phase2: {
+    id: 2,
     style: {
       backgroundImage: "url('./photo5.png')",
       "&::before": {
@@ -34,7 +59,7 @@ const phases = {
         left: 0,
         width: "100%",
         height: "100%",
-        backgroundImage: "url('./photo4.png')", 
+        backgroundImage: "url('./photo4.png')",
         backgroundSize: "cover",
         backgroundPosition: "center",
         opacity: 0,
@@ -45,43 +70,14 @@ const phases = {
         opacity: 0,
       },
     },
-    whileHover: { backgroundImage: "url('./photo3.png')" }, 
+    whileHover: { backgroundImage: "url('./photo3.png')" },
     title: "Branding",
-    text:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum libero enim nisi aliquam consectetur expedita magni eius ex corrupti animi! Ad nam pariatur assumenda quae mollitia libero repellat explicabo maiores?",
-  },
-  phase2: {
-    id: 2,
-    style: {
-      backgroundImage: "url('./photo5.png')", 
-      "&::before": {
-        content: "",
-        display: "block",
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        backgroundImage: "url('./photo4.png')", 
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        opacity: 0,
-        transition: "opacity 1s",
-        zIndex: 1,
-      },
-      "&:hover::before": {
-        opacity: 0,
-      },
-    },
-    whileHover: { backgroundImage: "url('./photo3.png')" }, 
-    title: "Branding",
-    text:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum libero enim nisi aliquam consectetur expedita magni eius ex corrupti animi! Ad nam pariatur assumenda quae mollitia libero repellat explicabo maiores?",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum libero enim nisi aliquam consectetur expedita magni eius ex corrupti animi! Ad nam pariatur assumenda quae mollitia libero repellat explicabo maiores?",
   },
   phase3: {
     id: 3,
     style: {
-      backgroundImage: "url('./photo2.png')", 
+      backgroundImage: "url('./photo2.png')",
       "&::before": {
         content: "",
         display: "block",
@@ -90,7 +86,7 @@ const phases = {
         left: 0,
         width: "100%",
         height: "100%",
-        backgroundImage: "url('./photo4.png')", 
+        backgroundImage: "url('./photo4.png')",
         backgroundSize: "cover",
         backgroundPosition: "center",
         opacity: 0,
@@ -103,13 +99,12 @@ const phases = {
     },
     whileHover: { backgroundImage: "url('./photo3.png')" },
     title: "Branding",
-    text:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum libero enim nisi aliquam consectetur expedita magni eius ex corrupti animi! Ad nam pariatur assumenda quae mollitia libero repellat explicabo maiores?",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum libero enim nisi aliquam consectetur expedita magni eius ex corrupti animi! Ad nam pariatur assumenda quae mollitia libero repellat explicabo maiores?",
   },
   phase4: {
     id: 4,
     style: {
-      backgroundImage: "url('./photo5.png')", 
+      backgroundImage: "url('./photo5.png')",
       "&::before": {
         content: "",
         display: "block",
@@ -118,7 +113,7 @@ const phases = {
         left: 0,
         width: "100%",
         height: "100%",
-        backgroundImage: "url('./photo4.png')", 
+        backgroundImage: "url('./photo4.png')",
         backgroundSize: "cover",
         backgroundPosition: "center",
         opacity: 0,
@@ -131,8 +126,7 @@ const phases = {
     },
     whileHover: { backgroundImage: "url('./photo3.png')" },
     title: "Branding",
-    text:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum libero enim nisi aliquam consectetur expedita magni eius ex corrupti animi! Ad nam pariatur assumenda quae mollitia libero repellat explicabo maiores?",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum libero enim nisi aliquam consectetur expedita magni eius ex corrupti animi! Ad nam pariatur assumenda quae mollitia libero repellat explicabo maiores?",
   },
 };
 
@@ -151,7 +145,7 @@ const Services = () => {
         animate={"animate"}
       >
         <motion.div className="textContainer" variants={variants}>
-          <p>JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN</p>
+          {/* <p>JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN</p> */}
           <hr />
         </motion.div>
         <motion.div className="titleContainer" variants={variants}>
