1253082ecb9b60e5cae6cb9ec63b9b7d54af50d0
Aman Gupta <amanguptaprogrammer@gmail.com>
2024-04-05 16:23:12 +0530

    safe

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,48 @@
 import Navbar from "./components/navbar/Navbar";
 import "./app.scss";
-import ImageCarousel from "./components/ImageCarousel/ImageCarousel"
+import ImageCarousel from "./components/ImageCarousel/ImageCarousel";
 import Contact from "./components/contact/Contact";
 import Hero from "./components/hero/Hero";
-
 
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
 import StarsBackground from "./components/StarBackground/StarsBackground";
 
-
 const App = () => {
   return (
     <>
+      <StarsBackground>
+        {/* <section id="Homepage">
+          <Navbar />
+          <Hero />
+        </section>
+        <section>
+          <Navbar />
+          <ImageCarousel />
+        </section>
 
-    {/* <StarsBackground>
-      
-    
-      <section id="Homepage">
-      
-<Navbar/>
-        <Hero />
-      </section>
-      <section>
-        
-<Navbar/>
-      <ImageCarousel/>
-      </section>
+        <section id="Services">
+          <Navbar />
+          <Parallax type="services" />
+        </section> */}
 
-      <section id="Services">
-        
-<Navbar/>
-        <Parallax type="services" />
-      </section>
+        <section>
+          <Navbar />
+          <Services />
+        </section>
+{/* 
+        <section id="Portfolio">
+          <Navbar />
+          <Parallax type="portfolio" />
+        </section>
 
-      <section>
-        
-<Navbar/> */}
-        <Services />
-      {/* </section>
+        <Portfolio />
 
-      <section id="Portfolio">
-        
-<Navbar/>
-        <Parallax type="portfolio" />
-      </section>
-
-      <Portfolio />
-
-      <section id="Contact">
-        <Contact />
-      </section>
-    </StarsBackground> */}
+        <section id="Contact">
+          <Contact />
+      </section> */}
+      </StarsBackground> 
     </>
   );
 };
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -21,6 +21,121 @@ const variants = {
   },
 };
 
+const item = {
+  phase1: {
+    id: 1,  
+    style: {
+      backgroundImage: "url('./photo5.png')",
+      "&::before": {
+        content: "",
+        display: "block",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        backgroundImage: "url('./photo4.png')", 
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        opacity: 0,
+        transition: "opacity 1s",
+        zIndex: 1,
+      },
+      "&:hover::before": {
+        opacity: 0,
+      },
+    },
+    whileHover: { backgroundImage: "url('./photo3.png')" }, 
+    title: "Branding",
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum libero enim nisi aliquam consectetur expedita magni eius ex corrupti animi! Ad nam pariatur assumenda quae mollitia libero repellat explicabo maiores?",
+  },
+  phase2: {
+    id: 2,
+    style: {
+      backgroundImage: "url('./photo5.png')", 
+      "&::before": {
+        content: "",
+        display: "block",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        backgroundImage: "url('./photo4.png')", 
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        opacity: 0,
+        transition: "opacity 1s",
+        zIndex: 1,
+      },
+      "&:hover::before": {
+        opacity: 0,
+      },
+    },
+    whileHover: { backgroundImage: "url('./photo3.png')" }, 
+    title: "Branding",
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum libero enim nisi aliquam consectetur expedita magni eius ex corrupti animi! Ad nam pariatur assumenda quae mollitia libero repellat explicabo maiores?",
+  },
+  phase3: {
+    id: 3,
+    style: {
+      backgroundImage: "url('./photo5.png')", 
+      "&::before": {
+        content: "",
+        display: "block",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        backgroundImage: "url('./photo4.png')", 
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        opacity: 0,
+        transition: "opacity 1s",
+        zIndex: 1,
+      },
+      "&:hover::before": {
+        opacity: 0,
+      },
+    },
+    whileHover: { backgroundImage: "url('./photo3.png')" },
+    title: "Branding",
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum libero enim nisi aliquam consectetur expedita magni eius ex corrupti animi! Ad nam pariatur assumenda quae mollitia libero repellat explicabo maiores?",
+  },
+  phase4: {
+    id: 4,
+    style: {
+      backgroundImage: "url('./photo5.png')", 
+      "&::before": {
+        content: "",
+        display: "block",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        backgroundImage: "url('./photo4.png')", 
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        opacity: 0,
+        transition: "opacity 1s",
+        zIndex: 1,
+      },
+      "&:hover::before": {
+        opacity: 0,
+      },
+    },
+    whileHover: { backgroundImage: "url('./photo3.png')" },
+    title: "Branding",
+    text:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum libero enim nisi aliquam consectetur expedita magni eius ex corrupti animi! Ad nam pariatur assumenda quae mollitia libero repellat explicabo maiores?",
+  },
+};
+
 const Services = () => {
   const ref = useRef();
 
@@ -49,7 +164,9 @@ const Services = () => {
         <motion.div className="listContainer" variants={variants}>
           <motion.div
             className="box"
-            whileHover={{ backgroundImage: `url(${photo2})` }}
+            style={item.phase1.style}
+            whileHover={item.phase1.whileHover}
+
           >
             <h2>Branding</h2>
             <p>
